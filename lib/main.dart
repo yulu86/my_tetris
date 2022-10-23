405d15e2a867f1263cce94b2a8daf366bca227e2
Yulu Xu <xuyulu86@126.com>
2022-10-23 12:23:39 +0800

    add i18n

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:my_tetris/gamer/gamer.dart';
-import 'package:my_tetris/gamer/keyboard.dart';
-import 'package:my_tetris/material/sound.dart';
-import 'package:my_tetris/panel/page_land.dart';
-import 'package:my_tetris/panel/page_portrait.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:my_tetris/generated/i18n.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,26 +13,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'tetris',
-      theme: ThemeData(
-        primaryColor: Colors.blue,
-      ),
-      home: const Scaffold(
-        body: Sound(
-          child: Game(
-            child: KeyboardController(child: _HomePage()),
-          ),
-        ),
-      ),
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
     );
-  }
-}
-
-class _HomePage extends StatelessWidget {
-  const _HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    bool land = MediaQuery.of(context).orientation == Orientation.landscape;
-    return land ? const PageLand() : const PagePortrait();
   }
 }
